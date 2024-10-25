470764f92d56cb2fa7c1ec481fd94c3d1de8065e
Ryazan8 <88ryazanov88@gmail.com>
2024-10-25 19:34:59 +0300

    Starts logic

--- src/stores/score.js
+++ src/stores/score.js
@@ -1,5 +1,32 @@
 import { defineStore } from 'pinia'
 
+const baseLevelScore = 25
+
+const levels = new Array(15).fill(0).map((_,  i) => baseLevelScore * Math.pow(2, i))
+
+const levelSores = levels.map((_, level) => {
+  let sum = 0
+
+  for (let [index, value] of levels.entries()) {
+    if (index >= level){
+      return sum + value
+    }
+    sum += value
+  }
+  return sum
+})
+
+function computeLevelByScore (score) {
+  for (let [index, value] of levelSores.entries()){
+    if(score <= value) {
+      return {
+        level: index,
+        value: levels[index]
+      }
+    }
+  }
+}
+
 export const useScoreStore = defineStore('score', {
   state: () => ({
     score: 0,
